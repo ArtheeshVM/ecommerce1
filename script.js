d55fbe3dcbcb99d2c7ea2c57a423cc953cd3150b
ArtheeshVM <717822p105@kce.ac.in>
2024-10-01 10:17:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+New! Keyboard shortcuts … Drive keyboard shortcuts have been updated to give you first-letters navigation
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+const close=document.getElementById('close');
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+    })
+}
+
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".login");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".form-section");
+
+signup.addEventListener("click", () => {
+    slider.classList.add("moveslider");
+    formSection.classList.add("form-section-move");
+});
+
+login.addEventListener("click", () => {
+    slider.classList.remove("moveslider");
+    formSection.classList.remove("form-section-move");
+});
